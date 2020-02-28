45c767a5186b9e9961739227f98bab42f86bab3e
luisdiogo98 <luisdiogo98@users.noreply.github.com>
2020-02-28 09:22:43 +0000

    Small block resizing

--- LBAW Joyride/Assets/Scripts/CameraMovement.cs
+++ LBAW Joyride/Assets/Scripts/CameraMovement.cs
@@ -34,20 +34,20 @@ public class CameraMovement : MonoBehaviour
     {
         this.transform.localPosition = new Vector3(this.transform.localPosition.x + speed, this.transform.localPosition.y, this.transform.localPosition.z);
         
-        if(this.transform.localPosition.x > 122.4f)
+        if(this.transform.localPosition.x > 102f)
         {
             for(int i = 0; i < blocks.Length; i++)
             {
                 if(i != pos) {
-                    blocks[i].transform.localPosition = new Vector3(blocks[i].transform.localPosition.x - 102.4f, blocks[i].transform.localPosition.y, blocks[i].transform.localPosition.z);
+                    blocks[i].transform.localPosition = new Vector3(blocks[i].transform.localPosition.x - 102f, blocks[i].transform.localPosition.y, blocks[i].transform.localPosition.z);
                 }
                 else {
-                    blocks[i].transform.localPosition = new Vector3(102.4f * 3, blocks[i].transform.localPosition.y, blocks[i].transform.localPosition.z);
+                    blocks[i].transform.localPosition = new Vector3(102f * 3, blocks[i].transform.localPosition.y, blocks[i].transform.localPosition.z);
                 }
             }
 
-            player.transform.localPosition = new Vector3(player.transform.localPosition.x - 102.4f, player.transform.localPosition.y, player.transform.localPosition.z);
-            this.transform.localPosition = new Vector3(this.transform.localPosition.x - 102.4f, this.transform.localPosition.y, this.transform.localPosition.z);
+            player.transform.localPosition = new Vector3(player.transform.localPosition.x - 102f, player.transform.localPosition.y, player.transform.localPosition.z);
+            this.transform.localPosition = new Vector3(this.transform.localPosition.x - 102f, this.transform.localPosition.y, this.transform.localPosition.z);
         
             pos++;
             if(pos > blocks.Length)
