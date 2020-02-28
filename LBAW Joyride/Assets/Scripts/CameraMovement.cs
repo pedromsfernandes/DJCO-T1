a4a40b019c611d1d11bdf2e5e3eb1768476396e9
luisdiogo98 <luisdiogo98@users.noreply.github.com>
2020-02-28 09:06:59 +0000

    Add camera collider adjustment to screen size ratio

--- LBAW Joyride/Assets/Scripts/CameraMovement.cs
+++ LBAW Joyride/Assets/Scripts/CameraMovement.cs
@@ -8,13 +8,25 @@ public class CameraMovement : MonoBehaviour
 
     public GameObject player;
     public GameObject[] blocks;
+    public float colliderColDepth = 4f;
+    public float colliderZPosition = 0f;
 
     int pos = 0;
+    Vector3 cameraPos;
+    Vector2 screenSize;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Generate world space point information for position and scale calculations
+        cameraPos = Camera.main.transform.position;
+        screenSize.x = Vector2.Distance (Camera.main.ScreenToWorldPoint(new Vector2(0,0)),Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0))) * 0.5f;
+        screenSize.y = Vector2.Distance (Camera.main.ScreenToWorldPoint(new Vector2(0,0)),Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height))) * 0.5f; 
+    
+        this.transform.Find("RightCollider").localScale = new Vector3(colliderColDepth, screenSize.y * 2, colliderColDepth);
+        this.transform.Find("RightCollider").position = new Vector3(cameraPos.x + screenSize.x + (this.transform.Find("RightCollider").localScale.x * 0.5f), cameraPos.y, colliderZPosition);
+        this.transform.Find("LeftCollider").localScale = new Vector3(colliderColDepth, screenSize.y * 2, colliderColDepth);
+        this.transform.Find("LeftCollider").position = new Vector3(cameraPos.x - screenSize.x - (this.transform.Find("LeftCollider").localScale.x * 0.5f), cameraPos.y, colliderZPosition);
     }
 
     // Update is called once per frame
