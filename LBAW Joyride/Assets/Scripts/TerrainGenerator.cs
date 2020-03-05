daea334964c8b53918410db27659933d6a581584
luisdiogo98 <luisdiogo98@users.noreply.github.com>
2020-03-05 18:25:54 +0000

    Add building texture

--- LBAW Joyride/Assets/Scripts/TerrainGenerator.cs
+++ LBAW Joyride/Assets/Scripts/TerrainGenerator.cs
@@ -9,6 +9,7 @@ public class TerrainGenerator : MonoBehaviour
     public int chunkHeight = 20;
 
     public GameObject unit;
+    public GameObject window;
     public GameObject artifact;
     public GameObject[] blocks;
 
@@ -46,6 +47,22 @@ public class TerrainGenerator : MonoBehaviour
                     newUnit.transform.localPosition = new Vector3(-51f + i, -10f + (chunkHeight - j), 0);
                     newUnit.SetActive(true);
                 }
+                else if (chunk[i, j] == -1)
+                {
+                    GameObject newWindow;
+                    if (transform.Find("WindowPool").childCount == 0)
+                    {
+                        newWindow = (GameObject)Instantiate(window);
+                    }
+                    else
+                    {
+                        newWindow = transform.Find("WindowPool").GetChild(0).gameObject;
+                    }
+
+                    newWindow.transform.parent = block.transform.Find("Items");
+                    newWindow.transform.localPosition = new Vector3(-51f + i, -10f + (chunkHeight - j), 0);
+                    newWindow.SetActive(true);
+                }
                 else if (chunk[i, j] == 2)
                 {
                     GameObject newArtifact;
@@ -66,17 +83,21 @@ public class TerrainGenerator : MonoBehaviour
     void EmptyBlock(GameObject block)
     {
         Transform items = block.transform.Find("Items");
-        while(items.childCount > 0)
+        while (items.childCount > 0)
         {
             items.GetChild(0).gameObject.SetActive(false);
 
-            if(items.GetChild(0).name.Contains("BasicBlock"))
+            if (items.GetChild(0).name.Contains("BasicBlock"))
             {
                 items.GetChild(0).parent = transform.Find("BlockPool");
             }
-            else
+            else if (items.GetChild(0).name.Contains("Artifact"))
             {
                 items.GetChild(0).parent = transform.Find("ArtifactPool");
+            }
+            else if (items.GetChild(0).name.Contains("Window"))
+            {
+                items.GetChild(0).parent = transform.Find("WindowPool");
             }
         }
     }
@@ -96,9 +117,9 @@ public class TerrainGenerator : MonoBehaviour
         currentHeight += floorHeight;
         for (int i = blockN; i < blockN + floorWidth; i++)
         {
-            for (int j = 0; j < currentHeight; j++)
+            for (int j = 0; j <= currentHeight; j++)
             {
-                chunk[i, chunkHeight - 1 - j] = 1;
+                GenerateBlock(chunk, chunkHeight, floorWidth, currentHeight, blockN, i, j);
             }
         }
         blockN += floorWidth;
@@ -149,7 +170,7 @@ public class TerrainGenerator : MonoBehaviour
                 {
                     for (int j = 0; j <= currentHeight; j++)
                     {
-                        chunk[i, chunkHeight - 1 - j] = 1;
+                        GenerateBlock(chunk, chunkHeight, floorWidth, currentHeight, blockN, i, j);
                     }
                 }
 
@@ -200,15 +221,38 @@ public class TerrainGenerator : MonoBehaviour
         if (currentHeight > 19) currentHeight = 19;
         for (int i = blockN; i < chunkWidth; i++)
         {
-            for (int j = 0; j < currentHeight; j++)
+            for (int j = 0; j <= currentHeight; j++)
             {
-                chunk[i, chunkHeight - 1 - j] = 1;
+                GenerateBlock(chunk, chunkHeight, floorWidth, currentHeight, blockN, i, j);
             }
         }
 
         if (debug) Print(chunk);
 
         return chunk;
+    }
+
+    void GenerateBlock(int[,] chunk, int chunkHeight, int floorWidth, int floorHeight, int blockN, int i, int j)
+    {
+        if (floorWidth < 3)
+            chunk[i, chunkHeight - 1 - j] = 1;
+        else
+        {
+            if (floorHeight % 2 != 0)
+            {
+                if (j % 2 == 0 && (blockN + i) % 2 != 0)
+                    chunk[i, chunkHeight - 1 - j] = -1;
+                else
+                    chunk[i, chunkHeight - 1 - j] = 1;
+            }
+            else
+            {
+                if (j % 2 != 0 && (blockN + i) % 2 != 0)
+                    chunk[i, chunkHeight - 1 - j] = -1;
+                else
+                    chunk[i, chunkHeight - 1 - j] = 1;
+            }
+        }
     }
 
     void Print(int[,] chunk)
