58ca441a751f26c848483c0172d4a4a425c26541
luisdiogo98 <luisdiogo98@users.noreply.github.com>
2020-03-05 15:05:16 +0000

    Add stub for enemy firing

--- LBAW Joyride/Assets/Scripts/Enemy.cs
+++ LBAW Joyride/Assets/Scripts/Enemy.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using rnd = UnityEngine.Random;
 
 public class Enemy : MonoBehaviour
 {
     bool moving = false;
+    bool firing = false;
     float startYPosition;
 
     float totalTime = 0;
+    float fireCooldownCounter;
+    public float fireCooldown = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        fireCooldownCounter = fireCooldown;
     }
 
     // Update is called once per frame
@@ -21,7 +25,27 @@ public class Enemy : MonoBehaviour
         if(moving)
         {
             totalTime += Time.deltaTime;
+            fireCooldownCounter -= Time.deltaTime;
             this.transform.localPosition = new Vector3(this.transform.localPosition.x, startYPosition * Mathf.Sin(totalTime), this.transform.localPosition.z);
+
+            if(fireCooldownCounter <= rnd.Range(-0.3f, 0.3f))
+            {
+                moving = false;
+                firing = true;
+                fireCooldownCounter = 0f;
+            }
+        }
+        else if(firing)
+        {
+            fireCooldownCounter += Time.deltaTime;
+            
+            //simulate firing
+            if(fireCooldownCounter >= 2f)
+            {
+                moving = true;
+                firing = false;
+                fireCooldownCounter = fireCooldown;
+            }
         }
     }
 
