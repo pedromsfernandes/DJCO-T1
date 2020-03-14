987cf118c26a62931c139ee33deff22f776e6bf2
luisdiogo98 <luisdiogo98@users.noreply.github.com>
2020-03-14 15:49:53 +0000

    Add stub for main menu

---
+++ LBAW Joyride/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    static bool first = true;
+
+    public GameObject canvas;
+    public Image logo;
+    public GameObject[] btns;
+
+    Vector2 screenSize;
+
+    void Start()
+    {
+        //Generate world space point information for position and scale calculations
+        screenSize.x = canvas.GetComponent<RectTransform>().sizeDelta.x;
+        screenSize.y = canvas.GetComponent<RectTransform>().sizeDelta.y;
+
+        logo.GetComponent<RectTransform>().sizeDelta = new Vector2(10000, screenSize.y / 3f);
+        logo.GetComponent<RectTransform>().localPosition = new Vector3(0, screenSize.y / 3f, 0);
+
+        if(first)
+            StartCoroutine("FirstMenuAnim");
+    }
+
+    IEnumerator FirstMenuAnim()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        for(int i = 0; i < 20; i++)
+        {
+            logo.color = new Color(logo.color.r, logo.color.g, logo.color.b, logo.color.a + 0.05f);
+            yield return new WaitForSeconds(0.03f);
+        }
+    }
+}
