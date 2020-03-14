da6b39f6626082ad90019a23302bec59d9ac9f88
luisdiogo98 <luisdiogo98@users.noreply.github.com>
2020-03-14 15:55:20 +0000

    Add audio sources

---
+++ LBAW Joyride/Assets/Scripts/SingleAudioSource.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+ 
+ public class SingleAudioSource : MonoBehaviour
+ {
+    private static AudioSource instance = null;
+    public static AudioSource Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this.transform.GetComponentInChildren<AudioSource>()) {
+            Destroy(this.gameObject);
+            return;
+        } else {
+            instance = this.transform.GetComponentInChildren<AudioSource>();
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    static public void PlayMusic()
+    {
+        if (instance.isPlaying) instance.Stop();
+        instance.Play();
+    }
+
+    static public void PlayMusic(AudioClip clip)
+    {
+        if(instance == null) return;
+        if (instance.isPlaying) instance.Stop();
+        instance.clip = clip;
+        instance.Play();
+    }
+ 
+    static public void StopMusic()
+    {
+        if(instance == null) return;
+        instance.Stop();
+    }
+ }
