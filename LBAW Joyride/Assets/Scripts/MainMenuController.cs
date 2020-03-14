603c0ea8db18bb94e1bd5f421adb29af9c45cfe7
luisdiogo98 <luisdiogo98@users.noreply.github.com>
2020-03-14 16:24:09 +0000

    Finished Main Menu

--- LBAW Joyride/Assets/Scripts/MainMenuController.cs
+++ LBAW Joyride/Assets/Scripts/MainMenuController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
@@ -22,18 +23,68 @@ public class MainMenuController : MonoBehaviour
         logo.GetComponent<RectTransform>().sizeDelta = new Vector2(10000, screenSize.y / 3f);
         logo.GetComponent<RectTransform>().localPosition = new Vector3(0, screenSize.y / 3f, 0);
 
-        if(first)
-            StartCoroutine("FirstMenuAnim");
+        float delta = 4.5f * screenSize.y / 9f;
+
+        for (int i = 0; i < btns.Length; i++)
+        {
+            btns[i].GetComponent<RectTransform>().sizeDelta = new Vector2(screenSize.y / 2.5f, screenSize.y / 9f);
+            btns[i].GetComponent<RectTransform>().localPosition = new Vector3(0, (-screenSize.y / 2f) - (screenSize.y / 9f) * (i + 1), 0);
+            btns[i].transform.Find("Text").GetComponent<Text>().fontSize = (int) (btns[i].GetComponent<RectTransform>().sizeDelta.y / 2f);
+        }
+
+        if (first)
+        {
+            StartCoroutine(FirstMenuAnim(delta));
+        }
     }
 
-    IEnumerator FirstMenuAnim()
+    public void NewGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void Controls()
+    {
+        SceneManager.LoadScene("ControlsMenu");
+    }
+
+    public void HighScores()
+    {
+        SceneManager.LoadScene("HighScoresMenu");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator FirstMenuAnim(float delta)
     {
         yield return new WaitForSeconds(0.5f);
 
-        for(int i = 0; i < 20; i++)
+        for (int i = 0; i < 20; i++)
         {
             logo.color = new Color(logo.color.r, logo.color.g, logo.color.b, logo.color.a + 0.05f);
             yield return new WaitForSeconds(0.03f);
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        for (int i = 0; i < btns.Length; i++)
+        {
+            StartCoroutine(ShowBtn(btns[i], delta));
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    IEnumerator ShowBtn(GameObject btn, float delta)
+    {
+        float step = delta / 20f;
+
+        for (int i = 0; i < 20; i++)
+        {
+            btn.transform.localPosition = new Vector3(btn.transform.localPosition.x, btn.transform.localPosition.y + step, btn.transform.localPosition.z);
+            yield return new WaitForSeconds(0.02f);
         }
     }
 }
