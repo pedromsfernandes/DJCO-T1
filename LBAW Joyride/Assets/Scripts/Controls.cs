009490adf912e2e83f865689f61d644b387dce2f
Pedro Fernandes <pedromiguel.fernandes17@gmail.com>
2020-03-16 15:45:35 +0000

    add controls menu

---
+++ LBAW Joyride/Assets/Scripts/Controls.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controls : MonoBehaviour
+{
+
+    public AudioClip buttonSound;
+
+    public void MainMenu()
+    {
+        SingleAudioSource.PlayMusic(buttonSound);
+        SceneManager.LoadScene("MainMenu");
+    }
+}
